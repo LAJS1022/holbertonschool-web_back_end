b7a1b4fa75ae0611b72abcf1dde2ee7d2a9d9563
LuisJimenez <luisssajs@gmail.com>
2026-05-07 16:08:53 +0000

    t6

---
+++ ES6_data_manipulation/6-set.js
@@ -0,0 +1,11 @@
+// 6-set.js
+
+/**
+ * Returns a Set created from an array.
+ *
+ * @param {Array} array - Input array of any type of elements
+ * @returns {Set} A Set containing the unique elements of the array
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}

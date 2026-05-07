19d97ca8381fce2ea64665bc59a0488724599134
LuisJimenez <luisssajs@gmail.com>
2026-05-07 15:49:58 +0000

    t0

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,13 @@
+// 0-get_list_students.js
+
+/**
+ * Returns a list of student objects
+ * Each student has: id (Number), firstName (String), location (String)
+ */
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+}

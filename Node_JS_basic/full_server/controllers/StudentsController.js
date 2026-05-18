4f0345b9edb859d3b2354d93c4e921fe004207bf
LuisJimenez <luisssajs@gmail.com>
2026-05-18 01:20:19 +0000

    t8

---
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,42 @@
+import { readDatabase } from '../utils';
+
+export default class StudentsController {
+  static getAllStudents(req, res) {
+    readDatabase(process.argv[2])
+      .then((fields) => {
+        const lines = ['This is the list of our students'];
+        const sorted = Object.keys(fields).sort((a, b) =>
+          a.toLowerCase().localeCompare(b.toLowerCase())
+        );
+
+        for (const field of sorted) {
+          lines.push(
+            `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`
+          );
+        }
+
+        res.status(200).send(lines.join('\n'));
+      })
+      .catch(() => {
+        res.status(500).send('Cannot load the database');
+      });
+  }
+
+  static getAllStudentsByMajor(req, res) {
+    const { major } = req.params;
+
+    if (major !== 'CS' && major !== 'SWE') {
+      res.status(500).send('Major parameter must be CS or SWE');
+      return;
+    }
+
+    readDatabase(process.argv[2])
+      .then((fields) => {
+        const students = fields[major] || [];
+        res.status(200).send(`List: ${students.join(', ')}`);
+      })
+      .catch(() => {
+        res.status(500).send('Cannot load the database');
+      });
+  }
+}
